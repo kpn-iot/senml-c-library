b5b2bdaed3d6ef89fdb2fc67d80e0fd5b537050f
PORTABLE\janbo <jbogaerts@myforce.be>
2018-04-15 19:37:46 +0200

    fix: improve rounding errors for printing doubles

--- senml_helpers.cpp
+++ senml_helpers.cpp
@@ -21,81 +21,36 @@ StreamContext* _streamCtx = NULL;
 
 void printDouble(double f, unsigned int digits) 
 {
-    int d;
-    static char s[26]; // formated number as string
-    int index = 0;
+    #ifdef __MBED__
+        char s[30];
+        sprintf(s, "%f", f);
+        printText(s, strlen(s));
+    #else
+        String temp(f, digits);
+        const char* s = temp.c_str();
+        int length = temp.length();
 
-    // handle sign
-    if (f < 0.0) {
-        s[index++] = '-';
-        f = -f;
-    }
-    // handle infinite values
-    if (isinf(f)) {
-        printText("INF", 3);
-        return;
-    }
-    // handle Not a Number
-    if (isnan(f)) {
-        printText("NaN", 3);
-        return;
-    }
-
-    // max digits
-    if (digits > 16) digits = 16;
-    int exponent = int(log10(f));
-    double g = f / pow(10, exponent);
-    if ((g < 1.0) && (g != 0.0)) {
-        g *= 10;
-        exponent--;
-    }
-    if (exponent < -330) { // lower limit of double-precision on Teensy 3
-        g = 0;
-        exponent = 0;
-    }
-    //if (digits < 16) { // display number rounded at last digit
-    //    g += 0.5 / pow(10, digits);
-    //}
-
-    d = g;
-    sprintf( & s[index++], "%d", d);
-    if(exponent < 4){
-        for(int i = 1; i <= exponent && i < 4; i++){    
-            g = (g - d) * 10.0; // shift one decimal place to the left
-            d = int(g);
-            sprintf( & s[index++], "%d", d);
+        for(int i = length -1; i >0; i--){                              //remove unwanted trailing 0
+            if(s[i] != '0'){
+                length = i;
+                if(s[i] == '.')                                         //if we end on something like x.  then add a last 0, so that it becomes x.0
+                    length++;
+                break;
+            }
         }
-        exponent -= (exponent > 4 ? 4 : exponent );
-    }
-    if (digits > 0) sprintf( & s[index++], ".");
-    for (int i = 0; i < digits; i++) {
-        g = (g - d) * 10.0; // shift one decimal place to the left
-        d = int(g);
-        sprintf( & s[index++], "%d", d);
-    }
-    for(int i = index -1; i >0; i--){                              //remove unwanted trailing 0
-        if(s[i] != '0'){
-            if(s[i] == '.')                                         //if we end on something like x.  then add a last 0, so that it becomes x.0
-                s[i+ 1] = '0';
-            break;
-        }
-        s[i] = 0;                                                   //set new end of string.
-    }
-    if(exponent != 0)                                               //only print the exponent if it has a meaning.
-        sprintf( & s[index], "E%+d", exponent);
-    printText(s, strlen(s));
-} // ===== end double2s()
+        printText(s, length + 1);
+    #endif
+} 
+
 
 
 void printBinaryAsBase64(const unsigned char* data, unsigned int length)
 {
-    int inputLen = sizeof(data);
-
     #ifdef ESP32
-        String encoded = base64::encode((uint8_t*)data, inputLen);
+        String encoded = base64::encode((uint8_t*)data, length);
         printText(encoded.c_str(), encoded.length());
     #else
-        int encodedLen = base64_enc_len(inputLen);
+        int encodedLen = base64_enc_len(length);
         char encoded[encodedLen];
         
         #ifdef __MBED__
@@ -104,7 +59,7 @@ void printBinaryAsBase64(const unsigned char* data, unsigned int length)
             mbedtls_base64_encode((unsigned char*)encoded, encodedLen, &olen, data, length);
         #else
             // note input is consumed in this step: it will be empty afterwards
-            base64_encode(encoded, (char*)data, inputLen); 
+            base64_encode(encoded, (char*)data, length); 
         #endif
         printText(encoded, encodedLen);
     #endif
